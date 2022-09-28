2c30d9989442ab5a1785cf1d690d371a58f3f375
jihk321 <jihk555@gmail.com>
2022-09-28 17:05:50 +0900

    2022_09_28_17:00

--- change.py
+++ change.py
@@ -1,4 +1,3 @@
-from genericpath import isfile
 import os
 import shutil
 import pandas as pd
@@ -9,63 +8,95 @@ file_path = now_path + '\\trash'
 file_list = os.listdir(now_path)
 file_name = [file for file in file_list if file.endswith('.csv')]
 file_jpg = [file for file in file_list if file.endswith('.jpg')]
-
+check = False
 files = pd.read_csv(now_path + '\\' +file_name[0], header=0, sep=',')
+files = files.sort_values('날짜')
 m_cnt, e_cnt, n_cnt = 0,0,0 # 삭제한 파일 갯수, 수정한 파일 갯수, 넘버링
+edit, move_name = 0,0
 
-move_file = pd.DataFrame(files[files['정답'].isnull()])
 if not os.path.isdir(file_path): os.mkdir(file_path)
-for idx, row in move_file.iterrows():
 
-    move_name = str(row['날짜']) + str(row['흥']) + '.jpg'
+for idx, row in files.iterrows():
 
-    try :
-        shutil.move(now_path +'\\'+ move_name, file_path +'\\'+ move_name)
-        m_cnt = m_cnt + 1
-    except :
-        remove_name = str(row['날짜'])  + '0' + str(row['흥']) + '.jpg'
-        if os.path.isfile(now_path + '\\' + remove_name) :
-            shutil.move(now_path +'\\'+ remove_name, file_path +'\\'+ remove_name)
+    if idx> 30 : break
+    if type(row['정답']) == float : 
+        move_name = file_jpg[idx]
+        try :
+            # shutil.move(now_path +'\\'+ move_name, file_path +'\\'+ move_name)
             m_cnt = m_cnt + 1
+        except :
+            print(f'error : {move_name}')
+    if row['검증'] == 1 : continue 
+    if row['검증'] == 0 and type(row['정답']) == str :
+        origin = file_jpg[idx]
+        edit = row['날짜'] + row['정답'] + '.jpg'
+        if os.path.isfile(now_path + '\\' + edit):
+            while check == True:
+                if os.path.isfile(now_path + '\\' +row['날짜'] + row['정답'] + '_0' + str(n_cnt) + '.jpg'): n_cnt = n_cnt + 1
+                elif not os.path.isfile(now_path + '\\' +row['날짜'] + row['정답'] + '_0' + str(n_cnt) + '.jpg'): check = True
+            edit = row['날짜'] + row['정답'] + '_0' + str(n_cnt) + '.jpg'
+            # print(edit)
+        try:
+            # os.rename(now_path+'\\'+origin,now_path+'\\'+edit)
+            n_cnt = 0
+        except Exception as e: print(e)
+        finally : e_cnt = e_cnt + 1 
+        check= False
+    print(f'move_name: {move_name}   origin : {origin} edit : {edit}')
+
+def remove():
+    move_file = pd.DataFrame(files[files['정답'].isnull()])
+    if not os.path.isdir(file_path): os.mkdir(file_path)
+    for idx, row in move_file.iterrows():
+
+        move_name = str(row['날짜']) + str(row['흥']) + '.jpg'
+
+        try :
+            shutil.move(now_path +'\\'+ move_name, file_path +'\\'+ move_name)
+            m_cnt = m_cnt + 1
+        except :
+            remove_name = str(row['날짜'])  + '0' + str(row['흥']) + '.jpg'
+            if os.path.isfile(now_path + '\\' + remove_name) :
+                shutil.move(now_path +'\\'+ remove_name, file_path +'\\'+ remove_name)
+                m_cnt = m_cnt + 1
     
-edit_file = pd.DataFrame(files[files['검증'].values == 0])
+def editname():
+    edit_file = pd.DataFrame(files[files['검증']].values == 0)
 
-for idx, row in edit_file.iterrows():
-    day = str(row['날짜'])
-    print(idx)
-    # print(len(file_jpg))
-    # origin_name = file_jpg[idx]
-    rename = day + str(row['정답']) + '.jpg'
+    for idx, row in edit_file.iterrows():
+        day = str(row['날짜'])
+        origin_name = row['날짜'] + row['흥'] + '.jpg'
+        rename = day + str(row['정답']) + '.jpg'
 
-    e_cnt = e_cnt + 1
-    # try :  os.rename(now_path+'\\'+origin_name,now_path+'\\'+rename)
-    # except :
-    #     while not os.path.isfile(now_path + '\\' + rename) :
-    #         n_cnt = n_cnt + 1 
-    #         rename = day = row['정답'] + '_' + str(n_cnt) + '.jpg'
-    #         print(rename)
-    #         os.rename(now_path+'\\'+origin_name,now_path+'\\'+rename)
-        
-    # finally : n_cnt = 0
-
-    # if os.path.isfile(now_path + '\\' + origin_name) :
-    #     e_cnt = e_cnt + 1
-    #     try : 
+        e_cnt = e_cnt + 1
+        # try :  os.rename(now_path+'\\'+origin_name,now_path+'\\'+rename)
+        # except :
+        #     while not os.path.isfile(now_path + '\\' + rename) :
+        #         n_cnt = n_cnt + 1 
+        #         rename = day = row['정답'] + '_' + str(n_cnt) + '.jpg'
+        #         print(rename)
+        #         os.rename(now_path+'\\'+origin_name,now_path+'\\'+rename)
             
-    #         rename = day + row['정답'] + '.jpg'
-    #         os.rename(now_path+'\\'+origin_name,now_path+'\\'+rename)
-    #         n_cnt = 0
-    #     except :
-    #         n_cnt = n_cnt + 1 
-    #         rename = day + row['정답'] + '_01.jpg'
-    #         print(day,row['정답'],n_cnt)
-    #         os.rename(now_path+'\\'+origin_name,now_path+'\\'+rename)
-    # else : 
-    #     origin_name = day + row['흥'] + '0'+'.jpg'
-    #     if os.path.isfile(now_path + '//' + origin_name) :
-    #         e_cnt = e_cnt + 1
+        # finally : n_cnt = 0
 
-    #         rename = day + str(row['정답']) +'01'+ '.jpg'
-    #         os.rename(now_path+'\\'+origin_name,now_path+'\\'+rename)
-        
+        if os.path.isfile(now_path + '\\' + origin_name) :
+            e_cnt = e_cnt + 1
+            try : 
+                
+                rename = day + row['정답'] + '.jpg'
+                os.rename(now_path+'\\'+origin_name,now_path+'\\'+rename)
+                n_cnt = 0
+            except :
+                n_cnt = n_cnt + 1 
+                rename = day + row['정답'] + '_01.jpg'
+                print(day,row['정답'],n_cnt)
+                os.rename(now_path+'\\'+origin_name,now_path+'\\'+rename)
+        else : 
+            if os.path.isfile(now_path + '\\' + day + '0' +row['흥'] +'.jpg') :
+                os.rename(now_path+'\\'+day + '0' +row['흥'] +'.jpg',now_path+'\\'+rename)
+            elif os.path.isfile(now_path + '\\' + day + '00' +row['흥'] +'.jpg') :
+                os.rename(now_path+'\\'+day + '00' +row['흥'] +'.jpg',now_path+'\\'+rename)
+            elif os.path.isfile(now_path + '\\' + day + '000' +row['흥'] +'.jpg') :
+                os.rename(now_path+'\\'+day + '000' +row['흥'] +'.jpg',now_path+'\\'+rename)
+
 print(f'{e_cnt}개 수정 {m_cnt}개 삭제 ')
--- rotate.py
+++ rotate.py
@@ -2,17 +2,18 @@ from email.mime import image
 import os
 from PIL import Image
 
-file_path = r'C:\Users\goback\Downloads\lpr\exp11\one_line'
+file_path = r'C:\Users\goback\Downloads\lpr\라벨링\exp11\one_line'
 file_list = os.listdir(file_path)
-save_path = file_path + '//save'
+file_jpg = [file for file in file_list if file.endswith('.jpg')]
+# save_path = file_path + '//save'
 
-if not os.path.isdir(save_path): os.mkdir(save_path)
+# if not os.path.isdir(save_path): os.mkdir(save_path)
 
-for i in file_list :
+for i in file_jpg :
     file_name = i
 
     img = Image.open(file_path + '\\' +file_name)
 
     r_img = img.transpose(Image.Transpose.ROTATE_180)
-    save_name = save_path + '\\' + i
+    save_name = file_path + '\\' + i
     r_img.save(save_name,'JPEG')
